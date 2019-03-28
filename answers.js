e030092698824361ec8c2200ce17ee1588309aa2
ProcneDC <procne@riseup.net>
2019-03-28 19:03:21 -0300

    Commit inicial

---
+++ answers.js
@@ -0,0 +1,23 @@
+var answers = [
+		{
+			answer: "Alcedinidae", 
+			filename: "001.jpg",
+			scientist_name: "Alcedo atthis",
+			name: "Martín pescador común",
+			state: "Precoupación menor"
+			info: [
+				"El martín pescador común o alción (Alcedo atthis) es una especie de ave coraciiforme de la familia Alcedinidae, que habita en lagos y ríos de Europa, África y Asia.",
+				"El pico es largo, constituyendo una parte nada despreciable de la longitud total del cuerpo (15,5-19 cm en total), de color negro con transición a naranja en la zona más cercana a la cabeza de la mandíbula inferior.",
+				"La envergadura alar ronda los 25 cm y el peso medio los 35 gramos.",
+			],
+			answer: "Furnariidae", 
+			filename: "002.jpg",
+			scientist_name: "Furnarius rufus",
+			name: "Hornero común",
+			state: "Precoupación menor"
+			info: [
+				"El hornero común es un ave insectívora que consigue su alimento mientras camina por el suelo.",				"El pico es largo, constituyendo una parte nada despreciable de la longitud total del cuerpo (15,5-19 cm en total), de color negro con transición a naranja en la zona más cercana a la cabeza de la mandíbula inferior.",
+				"Se trata de una especie monógina que construye un característico nido de barro en árboles, construcciones y otras estructuras.",			],
+			],
+		}
+	]
